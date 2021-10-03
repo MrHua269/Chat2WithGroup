1a540a8e5d870e92dfdbabfa7948490afd7b9f07
NatJerry <89722472+NatJerry@users.noreply.github.com>
2021-10-03 15:51:22 +0800

    Fuck BotInvite Into This Plugin

--- src/main/java/server/natural/Main.java
+++ src/main/java/server/natural/Main.java
@@ -5,10 +5,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import server.natural.ChatServer.ConnectHandler;
 import server.natural.ChatServer.InitChatServer;
+import server.natural.command.CommandInvite;
 import server.natural.command.CommandSMG;
 import server.natural.command.CommandSystemInfo;
 import server.natural.events.AddToTheGroup;
 import server.natural.events.RequestSelectorListener;
+import server.natural.events.onGroupMessage;
 
 
 import java.util.Objects;
@@ -22,12 +24,23 @@ public class Main extends JavaPlugin {
          Thread.sleep(3000);
          Bukkit.getPluginManager().registerEvents(new ConnectHandler(),this);
          InitChatServer.Init("0.0.0.0",Utils.config.getInt("ChatServerPort"));
+         boolean tmp = new Boolean(getConfig().getString("GroupToGame"));
+         boolean tmp1 = new Boolean(getConfig().getString("EnableInvite"));
          getLogger().info(ChatColor.LIGHT_PURPLE + "注册事件监听器...");
+         if(tmp == true){
+             Bukkit.getPluginManager().registerEvents(new onGroupMessage(), this);
+         }
+
+
          Bukkit.getPluginManager().registerEvents(new RequestSelectorListener(), this);
          Bukkit.getPluginManager().registerEvents(new AddToTheGroup(), this);
          getLogger().info(ChatColor.LIGHT_PURPLE + "注册相关指令");
          Objects.requireNonNull(Bukkit.getPluginCommand("smg")).setExecutor(new CommandSMG());
          Objects.requireNonNull(Bukkit.getPluginCommand("systeminfo")).setExecutor(new CommandSystemInfo());
+         if(tmp1 == true){
+             Objects.requireNonNull(Bukkit.getPluginCommand("botinvite")).setExecutor(new CommandInvite());
+
+         }
          getLogger().info(ChatColor.GREEN + "一切都准备好了！");
         }catch(Exception ignored){}
     }
---
+++ src/main/java/server/natural/events/onGroupMessage.java
@@ -0,0 +1,13 @@
+package server.natural.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class onGroupMessage implements Listener {
+    @EventHandler
+    public void onGroupMessageEvent(){
+
+    }
+
+
+}

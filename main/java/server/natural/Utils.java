add8d320b754f891e6a09d86fcceee6d538b35b8
NatJerry <89722472+NatJerry@users.noreply.github.com>
2021-10-05 21:11:42 +0800

    Delete main directory

--- main/java/server/natural/Utils.java
+++
@@ -1,6 +0,0 @@
-package server.natural;
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-public class Utils {
-    public static FileConfiguration config = Bukkit.getPluginManager().getPlugin("ChatWithGroup").getConfig();
-}

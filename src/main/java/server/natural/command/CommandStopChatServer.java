bb4bbd21b3f64ecf9ea85098a16ca078c709a229
wangxyper <79621885+wangxyper@users.noreply.github.com>
2022-01-22 11:39:35 +0000

    Update CommandStopChatServer.java

--- src/main/java/server/natural/command/CommandStopChatServer.java
+++ src/main/java/server/natural/command/CommandStopChatServer.java
@@ -6,14 +6,14 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import server.natural.ChatServer.BaseServer;
-
+import server.natural.Utils;
 public class CommandStopChatServer implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if (commandSender.isOp()){
             //Stop the chat server
             BaseServer.thread.interrupt();
-            Utils.executor2.shutdown();
+            Utils.executor2.shutdownNow();
             commandSender.sendMessage(ChatColor.RED+"聊天服务器暂停!");
         }
         return true;

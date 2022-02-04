a09824c15e438f845f92bdaa62c5e535f113def8
wangxyper <79621885+wangxyper@users.noreply.github.com>
2022-02-04 17:03:26 +0800

    Update CommandStopMessageTrasForwarding.java

--- src/main/java/server/natural/command/CommandStopMessageTrasForwarding.java
+++ src/main/java/server/natural/command/CommandStopMessageTrasForwarding.java
@@ -5,7 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import server.natural.events.onGroupMessage;
+import server.natural.events.OnGroupMessage;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -14,7 +14,7 @@ public class CommandStopMessageTrasForwarding implements CommandExecutor{
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if(commandSender instanceof Player){
             Player player = (Player)commandSender;
-            ConcurrentHashMap<Player,Boolean> forwardMap = onGroupMessage.getForwardMap();
+            ConcurrentHashMap<Player,Boolean> forwardMap = OnGroupMessage.getForwardMap();
             if (forwardMap.get(player)){
                 forwardMap.replace(player,false);
             }else{

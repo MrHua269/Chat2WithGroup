17972a5912b61d7cb097078af2abf97aba53ed46
wangxyper <79621885+wangxyper@users.noreply.github.com>
2022-02-04 13:44:30 +0800

    Update CommandStopMessageTrasForwarding.java

--- src/main/java/server/natural/command/CommandStopMessageTrasForwarding.java
+++ src/main/java/server/natural/command/CommandStopMessageTrasForwarding.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import server.natural.Utils;
 import server.natural.events.onGroupMessage;
 public class CommandStopMessageTrasForwarding implements CommandExecutor{
     @Override

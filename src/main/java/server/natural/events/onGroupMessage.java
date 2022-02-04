79b194e76e1f59cd6d63aa03f9e717b549ec2a31
wangxyper <79621885+wangxyper@users.noreply.github.com>
2022-02-04 13:18:24 +0800

    Update onGroupMessage.java

--- src/main/java/server/natural/events/onGroupMessage.java
+++ src/main/java/server/natural/events/onGroupMessage.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 public class onGroupMessage implements Listener {
-    public static ConcurrentMap<String,Boolean> booleans = new ConcurrentHashMap<>();
+    public static ConcurrentMap<String,Long> longs = new ConcurrentHashMap<>();
     @EventHandler
     public void onGroupMessageEvent(GroupMessageEvent event){
        Utils.executor.execute(()->{
@@ -22,7 +22,7 @@ public class onGroupMessage implements Listener {
                if(Utils.config.getBoolean("EnableGroupToGame")){
                    List<Player> players = (List<Player>) Bukkit.getOnlinePlayers();
                    for(Player player:players){
-                       if(booleans.get(player.getName())) {
+                       if(longs.get(player.getName())==1) {
                            player.sendMessage(ChatColor.BLUE + "[QQ]" + event.getEvent().getSenderName() + "(" + event.getUserID() + ")" + ":" + ChatColor.GRAY + event.getMsg());
                        }
                    }
@@ -33,14 +33,14 @@ public class onGroupMessage implements Listener {
     }
     @EventHandler
     public void onJoin(PlayerJoinEvent e){
-        if(!booleans.containsKey(e.getPlayer().getName())){
-            booleans.put(e.getPlayer().getName(),true);
+        if(!longs.containsKey(e.getPlayer().getName())){
+            longs.put(e.getPlayer().getName(),1);
         }
        
     }
     @EventHandler
     public void onQuit(PlayerQuitEvent e){
-        booleans.remove(e.getPlayer().getName());
+       longs.remove(e.getPlayer().getName());
     }
 
 }
